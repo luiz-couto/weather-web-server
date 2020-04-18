905e647a8f33c34d0312cbaad3a852d4de9e67ec
Luiz <luiz.felipe.556@outlook.com>
2020-04-18 14:43:24 -0300

    class 47

--- src/app.js
+++ src/app.js
@@ -4,7 +4,29 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = (path.join(__dirname, '../public'));
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index',{
+        title: 'Weather',
+        name: 'Luiz'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about',{
+        title: 'About me',
+        name: 'Luiz'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help',{
+        title: 'Help',
+        message: 'Some help message!'
+    });
+});
 
 app.get('/weather',(req, res) => {
     res.send({
