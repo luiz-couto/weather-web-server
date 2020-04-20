2f3c2cdcb7d74c21e3264838e064d061c75f27c1
Luiz <luiz.felipe.556@outlook.com>
2020-04-20 15:01:49 -0300

    class 59

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,29 @@
 console.log('Client side javascript!');
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
         if (data.error) {
-            console.log(data.error);
+            messageOne.textContent = '';
+            messageTwo.textContent = data.error;
             return;
         }
-        console.log(data.location);
-        console.log(data.forecastData);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = 'Info: ' + data.forecastData.weatherDes + '\n' + 'Temperature: ' + data.forecastData.temperature + ' °C\n' + 'FeelsLike: ' + data.forecastData.feelsLike + ' °C';
     })
-})
+});
+});
