83f0a75e738e07ce1b1cc0b3c63fbd75409f7e52
Luiz <luiz.felipe.556@outlook.com>
2020-04-20 13:31:49 -0300

    class 57

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Client side javascript!');
+
+fetch('http://localhost:3000/weather?address=boston').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error);
+            return;
+        }
+        console.log(data.location);
+        console.log(data.forecastData);
+    })
+})
